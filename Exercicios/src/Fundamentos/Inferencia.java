3ec08734ab5c33de5255d135b5cf7c54b85ec839
Luis_Felipe <luis_felipe_fc@hotmail.com>
2023-02-28 20:50:17 -0300

    Interferencia

---
+++ Exercicios/src/Fundamentos/Inferencia.java
@@ -0,0 +1,31 @@
+package Fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+
+		double A = 4.5;
+		System.out.println(A);
+		
+		A = 12;
+		System.out.println(A);
+		
+		var B = 4.5;
+		System.out.println(B);
+		
+		var C = "Texto";
+		System.out.println(C);
+		
+		C = "Outro Texto";
+		System.out.println(C);
+		
+		double D;				//Variavel foi declarada
+		D = 123.65;				//Variavel foi inicializada
+		System.out.println(D);	//Variavel foi usada
+		
+		var E = 123.45;
+		System.out.println(E);
+		
+		
+	}
+}
