52c9666a37e71dc7a62918db4e98778edb624cde
Luis_Felipe <luis_felipe_fc@hotmail.com>
2023-02-28 20:09:04 -0300

    comentarios

--- Exercicios/src/Fundamentos/PrimeiroProgama.java
+++ Exercicios/src/Fundamentos/PrimeiroProgama.java
@@ -3,7 +3,16 @@ package Fundamentos;
 public class PrimeiroProgama {
 
 	public static void main(String[] args) {
+		
+		//Uma sentença de código termina com ;
+		//Mais uma linha
 		System.out.println("Primeiro Programa Parte #01!");
+		
+		/*
+		 * Linha 1 
+		 * Linha 2
+		 * Linha 3
+		 */
 		System.out.println("Primeiro Programa Parte #02!");
 	}
 }
