29b7f4e91966a124899f839b5a8a5a019966e3a1
Luis_Felipe <luis_felipe_fc@hotmail.com>
2023-02-28 19:10:37 -0300

    First Commit

---
+++ Clinica/src/Tela/TelaPrincipal.java
@@ -0,0 +1,8 @@
+package Tela;
+
+public class TelaPrincipal {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Exercicios/src/Fundamentos/PrimeiroProgama.java
@@ -0,0 +1,9 @@
+package Fundamentos;
+
+public class PrimeiroProgama {
+
+	public static void main(String[] args) {
+		System.out.println("Primeiro Programa Parte #01!");
+		System.out.println("Primeiro Programa Parte #02!");
+	}
+}
