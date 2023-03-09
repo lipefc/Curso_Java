e638418d6d2663df295899aa379a77a0f96b17da
Luis_Felipe <luis_felipe_fc@hotmail.com>
2023-03-09 20:03:09 -0300

    Conversões do Tipo Primitivo para Numéricos

---
+++ Exercicios/src/Fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,22 @@
+package Fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+
+	public static void main(String[] args) {
+		
+		double a = 1.123456789; //Implícita
+		System.out.println(a);
+		
+		float b = (float) 1.123456789; //Explícita (CAST)
+		System.out.println(b);
+		
+		int c = 340; //Explícita (CAST)
+		byte d = (byte) c;
+		System.out.println(d);
+		
+		double e = 1.123456789; //Explícita (CAST)
+		int f = (int) e;
+		System.out.println(f);
+
+	}
+}
